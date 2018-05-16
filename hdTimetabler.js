5f46fece7b26bbeec2b0d5a478e2f751997d3dbc
devheedoo <devheedoo@gmail.com>
2018-05-16 18:03:14 +0900

    Create hdTimetabler.js

---
+++ hdTimetabler.js
@@ -0,0 +1,22 @@
+/**
+ * Example Codes
+ * 
+ * function TBlock() {
+ *   var title, id, prof, grade, credit, isLiberalArt;  // input values
+ *   var room, time;  // output values
+ * }
+ *
+ * function getAvailableTimes() {}
+ *
+ * function isAvailable() {}
+ *
+ * var lec0123 = new TBlock();
+ * lec0123.title = 'Lecture0123';
+ * lec0123.id = 'LEC01234';
+ * lec0123.prof = 'Gildong, Hong';
+ * lec0123.grade = 3;
+ * lec0123.credit = 3;
+ * lec0123.isLeberalArt = false;
+ *
+ * 
+ */
